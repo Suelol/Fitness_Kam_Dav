1c86c9c476fa2b9d09e8494d0d3fd4927dab0724
Suelol <kamalovramil432@gmail.com>
2024-12-02 13:59:37 +0300

    del

--- FitnessOsnova_Kam_Dav/DbModel/Users.cs
+++ FitnessOsnova_Kam_Dav/DbModel/Users.cs
@@ -24,6 +24,7 @@ namespace FitnessOsnova_Kam_Dav.DbModel
         public string Name { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Clients> Clients { get; set; }
